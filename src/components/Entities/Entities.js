61e0ca0e4f1ecd2cbaabffe93f00c49d78a238a6
DmitriyAV <dima.alekcee@gmail.com>
2023-08-16 17:58:32 +0300

    router react homework

--- src/components/Entities/Entities.js
+++ src/components/Entities/Entities.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Entity from './Entity';
 import './Entities.css';
-import { useParams } from 'react-router';
+import {useParams} from "react-router-dom";
 
-const Entities = ({ getCategoryData }) => {
-  const { category  } = useParams();
 
-  return (
-    <div className="entity-list">
-      {getCategoryData(category).map((entity, i) => (
-        <Entity key={i} category={category} name={entity.name} img={entity.img} level={entity.level} />
-      ))}
-    </div>
-  );
+const Entities = ({getCategoryData}) => {
+ const {category} = useParams()
+    return (
+        <div className="entity-list">
+            {getCategoryData(category).map((entity, i) => (
+                <Entity key={i} category={category} name={entity.name} img={entity.img} level={entity.level}/>
+            ))}
+        </div>
+    );
 }
 
 export default Entities;
--- src/components/Header/HogwartNavbar.js
+++ src/components/Header/HogwartNavbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './HogwartNavbar.css';
+import {Link} from "react-router-dom";
 
 const HogwartNavbar = () => {
   return (
     <nav className="navbar">
-        <div className="navbar-link">Home</div>
+        <Link to="/"> <div className="navbar-link">Home</div></Link>
         <div className="navbar-link">About</div>
     </nav>
   );
